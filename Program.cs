7d841b177e1358900e524814b393a89a20d95fae
unknown <maxxsarcilla@gmail.com>
2024-06-21 13:26:14 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace firstPRO
+
+{
+     class Program 
+     {
+        static void Main(string[] args)
+        {
+            string FirstName = "Macky";
+            string MiddleName = "Candelaria";
+            string LastName = "Sarcilla";
+            int Age = 24;
+            String Gender = "Female";
+
+            string FullName = ($"{FirstName} {MiddleName} {LastName}");
+
+            System.Console.WriteLine($"My Name is {FullName}. My Gender is {Gender}. My Age is {Age} years old.");
+        } 
+     }    
+}
